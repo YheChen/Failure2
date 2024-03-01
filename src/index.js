1c25f8095cbaad370eb5afdd3645953b1a70c768
YheChen <yanzhenchen123@gmail.com>
2024-03-01 10:00:41 -0500

    commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Sidescroll from "./pages/sidescroll";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Sidescroll />,
+  },
+  // {
+  //   path: "/",
+  //   element: None,
+  // },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
---
+++ src/pages/projects.js
@@ -0,0 +1,8 @@
+import "./projects.css";
+import React from "react";
+
+function Projects() {
+  return <div>ada</div>;
+}
+
+export default Projects;
